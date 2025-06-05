0c7ac7f464cf793959c95de5d7979bfe17fd2c31
Michael McCourt <mikemccourt1234@gmail.com>
2025-06-05 15:49:07 -0400

    calling getFileName

--- getFileName.js
+++ getFileName.js
@@ -1,5 +1,9 @@
 function getFileName() {
   let path = window.location.pathname;
   let fileName = path.substring(path.lastIndexOf('/') + 1);
-  document.getElementById('result').textContent = fileName;
+  let resultElem = document.getElementById('result');
+  if (resultElem) {
+    resultElem.textContent = fileName;
+  }
+  return fileName;
 }
